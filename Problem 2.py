816fb9e8d50a889c39aef6798a6b9af53f76e1d2
Gspin96 <higspin@gmail.com>
2022-04-21 00:10:58 +0200

    Renamed some variables and added comments
    Goodnight

--- Problem 2.py
+++ Problem 2.py
@@ -8,62 +8,62 @@ Created on Wed Apr 20 15:54:30 2022
 import math
 from classes.Objects import Line
 
-Plow = 1.25
-Pmax_base = 3.5
-Pchg = 0.055
+P_lo = 1.25     #Off-peak demand [MW]
+P_hi_base = 3.5 #Peak demand before chargers [MW]
+Pchg = 0.055    #Power per charger [MW]
 Nchg = 52
-disc_rate = 0.085
+P_hi = P_hi_base + Pchg * Nchg  #[MW]
 
-#High and low demand cost and hours per day
+disc_rate = 0.085 #8.5%
 
-Cl = 210
-Ch = 325
-Th = 6
-Tl = 24-Th
+#High and low demand cost and daily duration
+Cl = 210 #[NOK/MWh]
+Ch = 325 #[NOK/MWh]
+Th = 6      #[h]
+Tl = 24-Th  #[h]
 
 #Line characteristics:
 U=10 #kV
 Imax_25 = 0.255 #kA
-L = 8
-r = 18 #ohm*mm^2/km     resistivity
+Length = 8   #[km]
+resistivity = 18  #[ohm*mm^2/km]
 
 def problem1():
     print("\nProblem 1:")
-    Pmax = Pmax_base + Pchg * Nchg    #MW
     Plim_25 = math.sqrt(3)*U*Imax_25  #MW
 
-    print("Peak power: ",Pmax, " MW")
+    print("Peak power: ",P_hi, " MW")
     print("Base line capacity: ", Plim_25, " MW")
 
 def problem2():
     print("\nProblem 2:")
-    Pmax = Pmax_base + Pchg * Nchg
+    P_hi = P_hi_base + Pchg * Nchg
     
     #Option 1: FeAl75 60mm^2 line
-    FeAl75 = Line(U,r,L,60)
+    FeAl75 = Line(U,resistivity,Length,60)
     FeAl75.calc_inv_annuity(750000, disc_rate, 20) #
-    FeAl75.calc_op_cost(Plow, Pmax, Ch, Cl, Th)
+    FeAl75.calc_op_cost(P_lo, P_hi, Ch, Cl, Th)
     
-    Wloss_y = (FeAl75.loss(Plow)*Tl + FeAl75.loss(Pmax)*Th)*365 #Yearly energy loss
+    Wloss_y = (FeAl75.loss(P_lo)*Tl + FeAl75.loss(P_hi)*Th)*365 #Yearly energy loss
     
-    print("FeAl75 losses:")
-    print("\tYearly: ", Wloss_y, " MWh")
+    print("FeAl75:")
+    print("\tYearly loss: ", Wloss_y, " MWh")
     print("\to75: ", FeAl75.o, "NOK/y")
     print("\tf75: ", FeAl75.f, "NOK/y")
     print("\tc75: ", FeAl75.o + FeAl75.f, "NOK/y")
     
     #Option 2: FeAl90 90mm^2 line
-    FeAl90 = Line(U,r,L,90)
+    FeAl90 = Line(U,resistivity,Length,90)
     FeAl90.calc_inv_annuity(900000, disc_rate, 20)
-    FeAl90.calc_op_cost(Plow, Pmax, Ch, Cl, Th)
+    FeAl90.calc_op_cost(P_lo, P_hi, Ch, Cl, Th)
     
-    Wloss_y = (FeAl90.loss(Plow)*Tl + FeAl90.loss(Pmax)*Th)*365 #Yearly energy loss
+    Wloss_y = (FeAl90.loss(P_lo)*Tl + FeAl90.loss(P_hi)*Th)*365 #Yearly energy loss
     
-    print("\nFeAl90 losses:")
-    print("\tYearly: ", Wloss_y, " MWh")
-    print("\to75: ", FeAl90.o, "NOK/y")
-    print("\tf75: ", FeAl90.f, "NOK/y")
-    print("\tc75: ", FeAl90.o + FeAl90.f, "NOK/y")
+    print("\nFeAl90:")
+    print("\tYearly loss: ", Wloss_y, " MWh")
+    print("\to90: ", FeAl90.o, "NOK/y")
+    print("\tf90: ", FeAl90.f, "NOK/y")
+    print("\tc90: ", FeAl90.o + FeAl90.f, "NOK/y")
 
 problem1()
 problem2()
--- classes/Objects.py
+++ classes/Objects.py
@@ -11,25 +11,30 @@
 """
 
 class Line:
-    def __init__(self, U, r, L, A, Name="Line" ):
+    def __init__(self, U, resistivity, length, A, Name="Line" ):
         self.U = U
-        self.r = r
-        self.L = L
+        self.r = resistivity
+        self.L = length
         self.A = A
-        self.R = r*L/A
+        self.R = resistivity*length/A
         self.Name = Name
     
     #power loss from line at a given load P
     def loss(self, P):
         return self.R/(self.U**2)*P**2 
     
-    #yearly cost of operation for two daily load levels.
+    #Yearly cost of operation for two daily load levels
+    #   Pl: low demand power
+    #   Ph: high demand power
+    #   Ch: peak electricity cost
+    #   Cl: low electricity cost
+    #   Th: daily peak duration [h]
     def calc_op_cost(self, Pl, Ph, Ch, Cl, Th):
         self.o = 365*(self.loss(Pl)*Cl*(24-Th) + self.loss(Ph)*Ch*Th)
         return self.o
     
-    def annuity_factor(self,disc, period): 
-        return disc/(1-(1+disc)**(-period))
+    def annuity_factor(self,disc_rate, period): 
+        return disc_rate/(1-(1+disc_rate)**(-period))
     
     def calc_inv_annuity(self, Fkm, disc_rate, period):
         self.f = Fkm*self.L*self.annuity_factor(disc_rate, period)
